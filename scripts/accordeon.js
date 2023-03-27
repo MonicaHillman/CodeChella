a7e93fbeb3806bdacfa768f03926c2bcabfd58e5
monicahillman <mmhillman1@ucs.br>
2023-03-27 16:22:32 -0300

    :sparkles: feat: página informacoes

---
+++ scripts/accordeon.js
@@ -0,0 +1,19 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            panel.style.padding = '0 18px';
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            panel.style.padding = '2em 18px';
+        }
+    });
+}
